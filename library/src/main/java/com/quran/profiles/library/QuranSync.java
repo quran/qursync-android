09db0f1d5ea30e27dee4d8caa71a6b65da0f6a4b
ahmedre <ahmedre@gmail.com>
2014-08-22 01:30:21 -0700

    Code restructuring. Added some comments.

--- library/src/main/java/com/quran/profiles/library/QuranSync.java
+++ library/src/main/java/com/quran/profiles/library/QuranSync.java
@@ -128,6 +128,7 @@ public class QuranSync {
   public Observable<SyncResult> sync(final QuranSyncClient client) {
     Log.d(TAG, "sync");
 
+    // setup a map of bookmark ids to bookmarks (for existing bookmarks)
     final List<Bookmark> clientBookmarks = client.getBookmarks();
     final SparseArray<Bookmark> localBookmarksMap = new SparseArray<>();
     for (Bookmark bookmark : clientBookmarks) {
@@ -137,7 +138,21 @@ public class QuranSync {
       }
     }
 
+    // setup a map of tag ids to tags (for existing tags)
+    final List<Tag> clientTags = client.getTags();
+    final SparseArray<Tag> localTagsMap = new SparseArray<>();
+    for (Tag tag : clientTags) {
+      final Integer id = tag.getId();
+      if (id != null) {
+        localTagsMap.put(id, tag);
+      }
+    }
+
+    // list of deleted tags
+    final Set<Integer> deletedTagIds = client.getDeletedTagIds();
     final Set<Integer> deletedBookmarkIds = client.getDeletedBookmarkIds();
+
+    // bookmarks related api requests
     final Observable<List<Bookmark>> bookmarksObservable =
         mApi.getBookmarks()
             .flatMap(new Func1<List<Bookmark>, Observable<Bookmark>>() {
@@ -153,11 +168,14 @@ public class QuranSync {
                     Observable.from(client.getBookmarks())
                         .filter(new Func1<Bookmark, Boolean>() {
                           @Override
-                          public Boolean call(Bookmark bookmark) {
-                            return bookmark.getId() == null &&
-                                !serverBookmarks.contains(bookmark.getPointer());
+                          public Boolean call(Bookmark b) {
+                            return b.getId() == null &&
+                                !serverBookmarks.contains(b.getPointer());
                           }
                         });
+
+                // we will return an observable of the server bookmarks
+                // combined with any new client bookmarks
                 return Observable.from(bookmarkList)
                     .mergeWith(additions);
               }
@@ -168,22 +186,28 @@ public class QuranSync {
                 Log.d(TAG, "considering: " + b.toString());
                 final Integer id = b.getId();
                 if (id == null) {
+                  // this is a new client bookmark, add it to server
                   Log.d(TAG, "adding bookmark: " + b.toString());
                   return mApi.addBookmark(b);
                 } else if (deletedBookmarkIds.contains(id)) {
+                  // we explicitly deleted this on the client, so delete
                   Log.d(TAG, "deleting bookmark: " + b.toString());
                   return mApi.deleteBookmark(id);
                 } else {
+                  // check if we had this bookmark already
                   final Bookmark localBookmark = localBookmarksMap.get(id);
                   if (localBookmark != null && localBookmark.isUpdateOf(b)) {
+                    // we had the bookmark, but it changed...
                     Log.d(TAG, "updating bookmark: " + b.toString());
                     return mApi.updateBookmark(id, localBookmark);
                   } else {
+                    // bookmark is unchanged (or new on server)
                     return Observable.from(b);
                   }
                 }
               }
             })
+            // filter out the null bookmarks emitted by delete
             .filter(new Func1<Bookmark, Boolean>() {
               @Override
               public Boolean call(Bookmark bookmark) {
@@ -192,22 +216,24 @@ public class QuranSync {
             })
             .toList();
 
-    final List<Tag> clientTags = client.getTags();
-    final SparseArray<Tag> localTagsMap = new SparseArray<>();
-    for (Tag tag : clientTags) {
-      final Integer id = tag.getId();
-      if (id != null) {
-        localTagsMap.put(id, tag);
-      }
-    }
 
-    final Set<Integer> deletedTagIds = client.getDeletedTagIds();
+    // tags related api requests
     final Observable<List<Tag>> tagsObservable =
         mApi.getTags()
         .flatMap(new Func1<List<Tag>, Observable<Tag>>() {
           @Override
           public Observable<Tag> call(List<Tag> tags) {
-            return Observable.from(tags);
+            // we will return an observable containing the server tags
+            // combined with the new client tags
+            return Observable.from(tags)
+                .mergeWith(Observable.from(client.getTags())
+                    .filter(new Func1<Tag, Boolean>() {
+                      @Override
+                      public Boolean call(Tag tag) {
+                        return tag.getId() == null;
+                      }
+                    })
+                );
           }
         })
         .flatMap(new Func1<Tag, Observable<Tag>>() {
@@ -215,19 +241,24 @@ public class QuranSync {
           public Observable<Tag> call(Tag tag) {
             final Integer id = tag.getId();
             if (id == null) {
+              // this is a new tag
               return mApi.addTag(tag);
             } else if (deletedTagIds.contains(id)) {
+              // we explicitly deleted this tag
               return mApi.deleteTag(id);
             } else {
               final Tag clientTag = localTagsMap.get(id);
               if (clientTag != null && clientTag.isUpdateOf(tag)) {
+                // we have this tag, but we changed it
                 return mApi.updateTag(id, clientTag);
               } else {
+                // tag is unchanged (or new on server)
                 return Observable.from(tag);
               }
             }
           }
         })
+        // filter out the null tags emitted by delete
         .filter(new Func1<Tag, Boolean>() {
           @Override
           public Boolean call(Tag tag) {
@@ -235,6 +266,7 @@ public class QuranSync {
           }
         }).toList();
 
+    // run the bookmarks requests after tags is done
     return tagsObservable.flatMap(new Func1<List<Tag>, Observable<SyncResult>>() {
       @Override
       public Observable<SyncResult> call(List<Tag> tags) {
